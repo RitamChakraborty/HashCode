59272a2713eec08c8ef88e180e021b4093cd3ffe
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-02-06 19:43:37 +0530

    All possible orders for 2 members team

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -27,6 +26,29 @@ public class Main {
 
             for (int j = 0; j < count; ++j) {
                 pizzas[i].add(scanner.next());
+            }
+        }
+
+        int remaining = n;
+        Set<Integer> delivered = new HashSet<>();
+
+        for (int i = 0; i < t2; ++i) {
+            if (remaining >= 2) { // Check if enough pizzas are there to deliver to a 2 members team
+                int max = 0;
+                Map<int[], Set<String>> order = new HashMap<>();
+
+                for (int j = 0; j < n - 1; ++j) {
+                    Set<String> ingredients = new HashSet<>(pizzas[j]);
+
+                    for (int k = j + 1; k < n; ++k) {
+                        ingredients.addAll(pizzas[k]);
+                        order.put(new int[]{j, k}, ingredients);
+                    }
+                }
+
+                order.forEach((ints, strings) -> System.out.println(Arrays.toString(ints) + " : " + strings));
+            } else { // Otherwise cancel
+                break;
             }
         }
     }
